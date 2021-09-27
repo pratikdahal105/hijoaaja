1a1bcb67ef2714e8fe194fe64d2e6674a7f52796
pratikdahal105 <31059959+pratikdahal105@users.noreply.github.com>
2021-09-27 22:43:16 +0545

    carasoul caption

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -27,21 +27,15 @@
                 <div class="carousel-inner">
                     <div class="carousel-item active">
                         <img src="{{asset('assets/img/home-bg.jpg')}}" class="d-block w-100" alt="...">
-                        <div class="carousel-caption d-none d-md-block">
-                            <p>Some representative placeholder content for the first slide.</p>
-                        </div>
+                        <p class="font-weight-light">Some representative placeholder content for the first slide.</p>
                     </div>
                     <div class="carousel-item">
                         <img src="{{asset('assets/img/about.jpg')}}" class="d-block w-100" alt="...">
-                        <div class="carousel-caption d-none d-md-block">
-                            <p>Some representative placeholder content for the second slide.</p>
-                        </div>
+                        <p class="font-weight-light">Some representative placeholder content for the second slide.</p>
                     </div>
                     <div class="carousel-item">
                         <img src="{{asset('assets/img/home-bg.jpg')}}" class="d-block w-100" alt="...">
-                        <div class="carousel-caption d-none d-md-block">
-                            <p>Some representative placeholder content for the third slide.</p>
-                        </div>
+                        <p class="font-weight-light">Some representative placeholder content for the third slide.</p>
                     </div>
                 </div>
                 <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
