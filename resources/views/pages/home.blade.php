848bdd4391b5dfb92ecbbe4c1894c0661042501b
pratikdahal105 <31059959+pratikdahal105@users.noreply.github.com>
2021-10-02 23:02:59 +0545

    carasoule after first featured news

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -17,39 +17,10 @@
 <div class="container mt-5">
     <div class="row pt-5">
         <div class="col-md-7 col-lg-7 col-xl-7">
-            <h4>Gallery</h4>
-            <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
-{{--                <div class="carousel-indicators">--}}
-{{--                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>--}}
-{{--                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>--}}
-{{--                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>--}}
-{{--                </div>--}}
-                <div class="carousel-inner">
-                    <div class="carousel-item active">
-                        <img src="{{asset('assets/img/home-bg.jpg')}}" class="d-block w-100" alt="...">
-                        <p class="font-weight-light font-italic">Some representative placeholder content for the first slide.</p>
-                    </div>
-                    <div class="carousel-item">
-                        <img src="{{asset('assets/img/about.jpg')}}" class="d-block w-100" alt="...">
-                        <p class="font-weight-light font-italic">Some representative placeholder content for the second slide.</p>
-                    </div>
-                    <div class="carousel-item">
-                        <img src="{{asset('assets/img/home-bg.jpg')}}" class="d-block w-100" alt="...">
-                        <p class="font-weight-light font-italic">Some representative placeholder content for the third slide.</p>
-                    </div>
-                </div>
-                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Previous</span>
-                </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Next</span>
-                </button>
-            </div>
         @if($news_data[0] ?? null)
                 <h4 class="mt-2">Featured News</h4>
-            @foreach($news_data as $news)
+        @foreach($news_data as $key => $news)
+            @if($key == 0)
             <!-- Post preview-->
                 <div class="post-preview" id="{{$news->news->slug}}">
                     <a href="{{route('news.detail',$news->news->slug)}}">
@@ -67,6 +38,57 @@
                 </div>
                 <!-- Divider-->
                 <hr class="my-4" />
+
+                <h4>Gallery</h4>
+                <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+                    {{--                <div class="carousel-indicators">--}}
+                    {{--                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>--}}
+                    {{--                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>--}}
+                    {{--                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>--}}
+                    {{--                </div>--}}
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src="{{asset('assets/img/home-bg.jpg')}}" class="d-block w-100" alt="...">
+                            <p class="font-weight-light font-italic">Some representative placeholder content for the first slide.</p>
+                        </div>
+                        <div class="carousel-item">
+                            <img src="{{asset('assets/img/about.jpg')}}" class="d-block w-100" alt="...">
+                            <p class="font-weight-light font-italic">Some representative placeholder content for the second slide.</p>
+                        </div>
+                        <div class="carousel-item">
+                            <img src="{{asset('assets/img/home-bg.jpg')}}" class="d-block w-100" alt="...">
+                            <p class="font-weight-light font-italic">Some representative placeholder content for the third slide.</p>
+                        </div>
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
+                </div>
+                <hr class="my-4" />
+            @else
+                    <!-- Post preview-->
+                        <div class="post-preview" id="{{$news->news->slug}}">
+                            <a href="{{route('news.detail',$news->news->slug)}}">
+                                <h3 class="post-title">{{$news->news->title}}</h3>
+                                @if($news->news->thumbnail)
+                                    <img width="100%" src="{{asset('uploads/news/thumbnail/'.$news->news->thumbnail)}}">
+                                @endif
+                                <h4 class="post-subtitle">{{$news->news->summary}}</h4>
+                            </a>
+                            <p class="post-meta">
+                                Posted by
+                                <a href="#{{$news->news->slug}}">{{$news->news->author}}</a>
+                                on {{$news->news->publish_date}}
+                            </p>
+                        </div>
+                        <!-- Divider-->
+                        <hr class="my-4" />
+            @endif
         @endforeach
             <!-- Pager-->
         @else
