e50fd1dda3b9942fd300e98ced0b8719451a2ae0
pratikdahal105 <31059959+pratikdahal105@users.noreply.github.com>
2021-11-28 19:00:51 +0545

    advertisement

--- resources/views/pages/advertisement.blade.php
+++ resources/views/pages/advertisement.blade.php
@@ -1,11 +1,9 @@
-<?php
-    $ads = \App\Models\Advertisement::where('status', 1)->get();
-?>
+@php($ads = \App\Models\Advertisement::where('status', 1)->get())
 @foreach($ads as $ad)
-{{--    <a href="{{$ad->url}}">--}}
-        <img src="{{asset('uploads/ads/image/'.$ad->image)}}" class="d-block w-100" alt="...">
+    <a href="{{$ad->url}}" target="_blank">
+        <img src="{{asset('uploads/ads/image/'.$ad->image)}}" class="img-fluid" alt="{{$ad->image}}" />
         @if($ad->caption)
             <p>{{$ad->caption}}</p><br>
         @endif
-{{--    </a>--}}
+    </a>
 @endforeach
