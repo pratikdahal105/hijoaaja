aba8c66ddca5c30dfb3ad4c520e79e6acd4d8cda
pratikdahal105 <31059959+pratikdahal105@users.noreply.github.com>
2021-12-01 12:39:20 +0545

    random 5

--- resources/views/pages/advertisement.blade.php
+++ resources/views/pages/advertisement.blade.php
@@ -1,4 +1,4 @@
-@php($ads = \App\Models\Advertisement::where('status', 1)->get())
+@php($ads = \App\Models\Advertisement::where('status', 1)->inRandomOrder()->limit(5)->get())
 @foreach($ads as $ad)
     <a href="{{$ad->url}}" target="_blank">
         <img src="{{asset('uploads/ads/image/'.$ad->image)}}" class="img-fluid" alt="{{$ad->image}}" />
