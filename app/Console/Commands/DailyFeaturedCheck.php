7bc6a3d9b86677935e3f6becae3f1a116835421a
pratikdahal105 <31059959+pratikdahal105@users.noreply.github.com>
2021-12-07 20:06:46 +0545

    sheduled task

--- app/Console/Commands/DailyFeaturedCheck.php
+++ app/Console/Commands/DailyFeaturedCheck.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands;
 use App\Models\Featured;
 use Carbon\Carbon;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
 
 class DailyFeaturedCheck extends Command
 {
@@ -46,6 +47,7 @@ class DailyFeaturedCheck extends Command
                 $feature->delete();
             }
         }
+        Log::info('Featured Cleared');
         return 0;
     }
 }
